1569797ade3c9189e6eb80e80a7c7f34da29a34b
Darshan3338 <154723766+Darshan3338@users.noreply.github.com>
2025-03-24 19:49:45 +0530

    Update assets.js

--- frontend/src/assets/frontend_assets/assets.js
+++ frontend/src/assets/frontend_assets/assets.js
@@ -60,6 +60,8 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 import foodlogo from './foodlogo.jpg'
+import insta from './insta.png'
+import Gmail from './Gmail.png'
 
 export const assets = {
     logo,
@@ -81,7 +83,9 @@ export const assets = {
     logout_icon,
     bag_icon,
     parcel_icon,
-    foodlogo
+    foodlogo,
+    insta,
+    Gmail
 }
 
 export const menu_list = [
